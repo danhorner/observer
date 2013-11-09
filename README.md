583c53f546724a799d2516b18634be47109aac83
Daniel Horner <daniel_horner@pobox.com>
2013-11-08 16:49:49 -0800

    - Add variable operations v1+v2 - blocked variables return pending value

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 observer
 ========
-
-Python observer class. Similar to py-notify but simpler. Provides a `Variable` class with support for blocking, and an `Algorithm` class that coalesces updates from blocked inputs
-
 This is a small observer library. There are many, but this one meets my particular needs:
 
 - Straightforward encapsulation of Algorithms that run when input conditions are changed
 - Variables can be blocked to suppress updates until inputs have stabilized
-	- Never worry about callback ordering
+    - Never worry about callback ordering
 - Observer syntax should be readable
 
 See esp: 
@@ -15,26 +12,63 @@ See esp:
 - [py-notify](http://home.gna.org/py-notify/)              (Close, but I esp. want to coalesce updates)
 - [trellis](https://pypi.python.org/pypi/Trellis/0.7a2)    (Looks promising, if heavy)
 
+Sneak Preview:
 
-`Observable` exposes the special property `value`, which is implemented by get() and set()
-
-```pycon
->>> def pp(name):
+'''pycon
+>>> def printVar(name):
 ...     def p(x):
 ...        print "%s: %r"%(name,x)
 ...     return p
+'''
 
+Define a Variable:
+
+'''pycon
+     >>> v1=Variable(3)
+'''
+
+Make a derived variable:
+
+'''pycon
+>>> v2= (v1+3)/5
+>>> v2.observe(printVar("v2"))
+'''
+
+Watch as changes propagate to the derived variable
+
+'''pycon
+>>> v1.value=27
+v2: 6
+'''
+
+It also works when combining two variables
+
+'''pycon
+>>> v3 = v2 * 2
+>>> v3.observe(printVar("v3"))
+>>> v1.value= 12
+v2: 3
+v3: 6
+'''
+
+### Basic Objects ###
+
+`Observable` exposes the special property `value`, which is implemented by get() and set(). It also implements observe() 
+
+'''pycon
 >>> o=Observable(1)
->>> o.observe(pp("o"))
+>>> o.observe(printVar("o"))
 >>> o.value=2
 o: 2
-```
+'''
 
-`Variable` is like `Observable`, but also has an observable `Blocked` flag
+`Variable` is like `Observable`, but updates can be suppressed using the
+`blocked` flag, which is also observable. blocking is essential to have updates
+propagate correctly when there are diamond-shaped Flows: v1->(v2,v3)->v4
  
-```pycon
+'''pycon
 >>> v=Variable()
->>> v.observe(pp("v"))
+>>> v.observe(printVar("v"))
 >>> v.value=3
 v: 3
 >>> v.block()
@@ -42,48 +76,47 @@ v: 3
 >>> v.value=5
 >>> v.unblock()
 v: 5
-```
+'''
 
 Variables can track other variables unidirectionally
 
-```pycon
+'''pycon
 >>> source=Variable(2)
 >>> v.track_variable(source)
 v: 2
 >>> source.value=7
 v: 7
-```
+'''
 
 Variables can be linked with other variables bidirectionally
 
-```pycon
+'''pycon
 >>> v_copy=Variable()
->>> v_copy.observe(pp("v_copy"))
+>>> v_copy.observe(printVar("v_copy"))
 >>> linkVariables(v,v_copy)
 v_copy: 7
 >>> source.value="two places at once"
 v: 'two places at once'
 v_copy: 'two places at once'
 >>> unlinkVariables(v,v_copy)
-```
-
+'''
 
 `Algorithm` is a container for variables that connects inputs to outputs
 
-```pycon
+'''pycon
 >>> class Inverter(Algorithm):
 ...     _inputs_=('input',)
 ...     _outputs_=('output',)
 ...     def update(self):
 ...         self.output.value=not self.input.value
 
->>> i=Inverter()
->>> i.output.observe(pp("inverted"))
+>>> i=Inverter(enabled=False)
+>>> i.output.observe(printVar("inverted"))
 >>> i.enabled.set(True)
 inverted: True
 >>> i.input.value=True
 inverted: False
-```
+'''
 
 These modules can be connected together to form a flow graph. The idea is to
 use the `blocked` flag to suppress processing until the parent data has stopped
@@ -91,4 +124,4 @@ flapping about. There is also an `enable` flag which is useful to stop your
 algorithm from executing until the inputs are connected.
 
 ** Todo: ** I don't care about weak references for now. At some point I may add them, but 
-
+** Todo: ** Make block/unblock operations threadsafe
--- observer.py
+++ observer.py
@@ -26,7 +26,7 @@ This is a small observer library. There are many, but this one meets my particul
 
 - Straightforward encapsulation of Algorithms that run when input conditions are changed
 - Variables can be blocked to suppress updates until inputs have stabilized
-	- Never worry about callback ordering
+    - Never worry about callback ordering
 - Observer syntax should be readable
 
 See esp: 
@@ -34,23 +34,46 @@ See esp:
 - [py-notify](http://home.gna.org/py-notify/)              (Close, but I esp. want to coalesce updates)
 - [trellis](https://pypi.python.org/pypi/Trellis/0.7a2)    (Looks promising, if heavy)
 
+Sneak Preview:
 
-`Observable` exposes the special property `value`, which is implemented by get() and set()
-
-     >>> def pp(name):
+     >>> def printVar(name):
      ...     def p(x):
      ...        print "%s: %r"%(name,x)
      ...     return p
 
+Define a Variable:
+     >>> v1=Variable(3)
+
+Make a derived variable:
+     >>> v2= (v1+3)/5
+     >>> v2.observe(printVar("v2"))
+
+Watch as changes propagate to the derived variable
+     >>> v1.value=27
+     v2: 6
+
+It also works when combining two variables
+     >>> v3 = v2 * 2
+     >>> v3.observe(printVar("v3"))
+     >>> v1.value= 12
+     v2: 3
+     v3: 6
+
+### Basic Objects ###
+
+`Observable` exposes the special property `value`, which is implemented by get() and set(). It also implements observe() 
+
      >>> o=Observable(1)
-     >>> o.observe(pp("o"))
+     >>> o.observe(printVar("o"))
      >>> o.value=2
      o: 2
 
-`Variable` is like `Observable`, but also has an observable `Blocked` flag
+`Variable` is like `Observable`, but updates can be suppressed using the
+`blocked` flag, which is also observable. blocking is essential to have updates
+propagate correctly when there are diamond-shaped Flows: v1->(v2,v3)->v4
  
      >>> v=Variable()
-     >>> v.observe(pp("v"))
+     >>> v.observe(printVar("v"))
      >>> v.value=3
      v: 3
      >>> v.block()
@@ -70,7 +93,7 @@ Variables can track other variables unidirectionally
 Variables can be linked with other variables bidirectionally
 
     >>> v_copy=Variable()
-    >>> v_copy.observe(pp("v_copy"))
+    >>> v_copy.observe(printVar("v_copy"))
     >>> linkVariables(v,v_copy)
     v_copy: 7
     >>> source.value="two places at once"
@@ -87,8 +110,8 @@ Variables can be linked with other variables bidirectionally
     ...     def update(self):
     ...         self.output.value=not self.input.value
      
-    >>> i=Inverter()
-    >>> i.output.observe(pp("inverted"))
+    >>> i=Inverter(enabled=False)
+    >>> i.output.observe(printVar("inverted"))
     >>> i.enabled.set(True)
     inverted: True
     >>> i.input.value=True
@@ -100,13 +123,27 @@ flapping about. There is also an `enable` flag which is useful to stop your
 algorithm from executing until the inputs are connected.
 
 # ** Todo: ** I don't care about weak references for now. At some point I may add them, but 
+# ** Todo: ** Make block/unblock operations threadsafe
 """
 
-__all__=('Algorithm','Variable','Observable','linkVariables','unlinkVariables')
+__all__=(
+
+'Observable', 'Variable', 'IdentityVariable', 'AlwaysUpdateVariable',
+
+'Algorithm', 'Add', 'Subtract', 'Multiply', 'Divide',
+
+'linkVariables', 'unlinkVariables',
+
+'variable_operation',
+
+'any_var_is_none','debugVariable', 'pp',
+
+)
 
 
 import functools
 import operator
+
 from contextlib import contextmanager
 
 #Set __DEBUG__ to true to track the nesting level. This is a bit slower. The pretty-printing function can use it 
@@ -119,7 +156,7 @@ def adderExample():
     """
     class Adder(Algorithm):
         _inputs_=('a','b')
-        _outputs_=('c')
+        _outputs_=('c',)
         def __init__(self,name):
             self.name=name
             self.a=Variable(0)
@@ -207,7 +244,13 @@ class Observable(object):
         return self._value
     
     def set(self,value):
-        if not self.equality_test(self._value,value):
+        try:
+            changed=not self.equality_test(self._value,value)
+        except:
+            print "Equality Test Error in class %r: (%r),(%r)"%(self.__class__,self._value,value)
+            raise
+
+        if changed:
             self._value=value
             self.notify_observers()
 
@@ -327,6 +370,12 @@ class Variable(Observable):
         else:
              self._set(value)
                 
+    def get(self):
+        if self.blocked.value:
+            return self.pendingValue
+        else:
+            return super(Variable,self).get()  
+
     def track_variable(self,sourceVar):
         sourceVar.blocked.observe(self.setBlocked)
         sourceVar.observe(self.set)
@@ -337,22 +386,46 @@ class Variable(Observable):
         sourceVar.unobserve(self.set)
         self.blocked.value=False
                            
-    value = property(Observable.get,set) 
+    def __add__(self,x):
+        return variable_operation(Add,self,x)
+    def __sub__(self,x):
+        return variable_operation(Subtract,self,x)
+    def __mul__(self,x):
+        return variable_operation(Multiply,self,x)
+    def __div__(self,x):
+        return variable_operation(Divide,self,x)
+
+    value = property(get,set) 
         
-        
-    # Special constructors to flag attributes when defining a container
-    # The container metaclass will look for these and stitch together the 
-    # Valid logic
-    @classmethod
-    def INPUT(cls):
-        return cls()
-    
-    @classmethod
-    def OUTPUT(cls):
-        return cls()
+class IdentityVariable(Variable):
+    """
+    >>> a1=["123"]
+    >>> a2=["123"]
+    >>> b1="123"
+    >>> b2="123"
+    >>> v1=IdentityVariable()
+    >>> debugVariable(v1,"v1")
+    >>> v1.value=a2
+    v1 value: ['123']
+    >>> v1.value=a1
+    v1 value: ['123']
+    >>> v1.value=b1
+    v1 value: '123'
+    >>> v1.value=b2
+    """
+    equality_test=operator.is_
 
+class AlwaysUpdateVariable(Variable):
+    """
+    >>> v1=AlwaysUpdateVariable()
 
-
+    >>> debugVariable(v1,"v1")
+    >>> v1.value=3
+    v1 value: 3
+    >>> v1.value=3
+    v1 value: 3
+    """
+    equality_test=lambda self,a,b: False
 def linkVariables(v1,v2):
     """
     Create a bidirectional link between v1 and v2.
@@ -363,7 +436,7 @@ def linkVariables(v1,v2):
 
     The variables are blocked as a group.
 
-    Note that it's not safe to link variables where equality_test always returns True. 
+    Note that it's not safe to link variables where equality_test always returns False. 
     Note that linking variables creates a cycle. if you don't unlink them later, you will leak memory
 
         Define a pretty printer for this example 
@@ -457,7 +530,10 @@ class Algorithm(object):
     In either case, it will bind  inputs to call the update() function on change
     It will also bind the valid flags to update into a central _valid_ object on the instance. 
 
-    There is a central enable() flag that is set to false by default. This is so that you can bind values into the inputs before running the algorithm
+    There is a central enable() flag that is set to true by default.
+    If you need to bind values into the inputs before running the algorithm,
+    then set it to true in the constructor or by setting the class variable
+    `_start_enabled_`
 
         Define a pretty printer for this example 
         >>> def p(name):
@@ -468,12 +544,13 @@ class Algorithm(object):
         Your code goes in a subclass of Algorithm
         >>> class  Adder(Algorithm):
         ...     _inputs_=('a','b')
-        ...     _outputs_=('c')
+        ...     _outputs_=('c',)
+        ...     _start_enabled_=False
         ...
         ...     def update(self):
         ...         self.c.value = self.a.value + self.b.value
 
-        Input and output variables are automatically creaated
+        Input and output variables are automatically created
         >>> adder=Adder()
         >>> adder.a.observe(p("a"))
         >>> adder.b.observe(p("b"))
@@ -498,30 +575,50 @@ class Algorithm(object):
         AttributeError: 'NoneType' object has no attribute 'value'
 
     """
-    __slots__=("_inputs_","_outputs_","updatePending","enabled","outputs_blocked")
+    __slots__=("_inputs_","_outputs_","inputs","outputs","enabled","outputs_blocked")
     __variableType__=Variable
+    _start_enabled_=True
+    _outputs_=tuple()
+    _inputs_=tuple()
     
-    def __init__(self,enabled=False):
+    def __init__(self,enabled=None,**kwargs):
+        if enabled is None:
+            enabled=self._start_enabled_
+
         self.updatePending=False
         self.outputs_blocked=Observable(False)
         
         self.enabled=Observable(enabled)
         self.enabled.observe(self.check_blocks_and_update)
         
+        assert isinstance(self._inputs_,(tuple,list))
         for attrName,constructor in _get_variable_constructors(self._inputs_):
-            inputVariable=getattr(self,attrName,None) or constructor()
+            if hasattr(self,attrName):
+                inputVariable=getattr(self,attrName,None)
+            elif attrName in kwargs: 
+                inputVariable=constructor(kwargs[attrName])
+            else:
+                inputVariable=constructor()
+
             setattr(self,attrName,inputVariable)
             
             inputVariable.blocked.observe(self.check_blocks_and_update)
             inputVariable.observe(self.check_blocks_and_update)
 
-            
+        assert isinstance(self._outputs_,(tuple,list))
         for attrName,constructor in _get_variable_constructors(self._outputs_):  
-            outputVariable=getattr(self,attrName,None) or constructor()
+            if hasattr(self,attrName):
+                outputVariable=getattr(self,attrName,None)
+            elif attrName in kwargs: 
+                outputVariable=constructor(kwargs[attrName])
+            else:
+                outputVariable=constructor()
             setattr(self,attrName,outputVariable)
             
             self.outputs_blocked.observe(outputVariable.setBlocked)
-            
+
+        self.inputs=[getattr(self,n) for n,t in _get_variable_constructors(self._inputs_) ]
+        self.outputs=[getattr(self,n) for n,t in _get_variable_constructors(self._outputs_) ]
             
         self.check_blocks_and_update()
    
@@ -529,13 +626,13 @@ class Algorithm(object):
         pass
     
     def check_blocks_and_update(self,dummy=None):
-        isBlocked= not(self.enabled.value) or any(map(lambda i: getattr(self,i).blocked.value, self._inputs_))      
+        isBlocked= not(self.enabled.value) or any(map(lambda i: i.blocked.value, self.inputs))      
         if not isBlocked:
             self.update()
 
         self.outputs_blocked.value = isBlocked
-
         
+
     def observe(self,attribute,callback):
         getattr(self,'_' + attribute).observe(callback)
         
@@ -549,6 +646,102 @@ class Algorithm(object):
     def set(varname,self,value):
         variable=getattr(self,varname)
         variable.value=value
+
+def pp(name):
+    def p(x):
+        print "%s%s: %r"%(('-')*_nest_level,name,x)
+        return x
+    return p
+    
+def debugVariable(v,name):
+    def blockWatch(b):
+        assert(v.equality_test(v.value,v.pendingValue)) # Someone tried to manipulate blocked flag directly
+        print (('-') * _nest_level) + name + " blocked: " + repr(b) + (" value: [%r]"%(v.value,))
+    v.observers.insert(0,pp(name + " value"))
+    v.blocked.observers.insert(0,blockWatch)
+    
+
+
+
+class Subtract(Algorithm):
+    """
+    >>> sub=Subtract(a=1,b=3)
+    >>> sub.a.value
+    1
+    >>> sub.b.value
+    3
+    >>> sub.c.value
+    -2
+    """
+    _inputs_=('a','b')
+    _outputs_=('c',)
+    def update(self):
+        if any_var_is_none(self.inputs):
+            self.c.value=None
+        else:
+            self.c.value=(self.a.value - self.b.value)
+
+class Add(Algorithm):
+    _inputs_=('a','b')
+    _outputs_=('c',)
+    def update(self):
+        if any_var_is_none(self.inputs):
+            self.c.value=None
+        else:
+            self.c.value=self.a.value + self.b.value
+
+class Multiply(Algorithm):
+    _inputs_=('a','b')
+    _outputs_=('c',)
+    def update(self):
+        if any_var_is_none(self.inputs):
+            self.c.value=None
+        else:
+            self.c.value=self.a.value * self.b.value
+
+class Divide(Algorithm):
+    _inputs_=('a','b')
+    _outputs_=('c',)
+    def update(self):
+        if any_var_is_none(self.inputs):
+            self.c.value=None
+        else:
+            self.c.value=self.a.value / self.b.value
+
+def variable_operation(algorithm,*inputs):
+    """
+
+    Doesn't handle IdentityVariable yet: That is, the inputs are not of the
+    correct type, so identity_operation will throw on numpy arrays
+
+    >>> v1=Variable(3)
+    >>> v2=Variable(1)
+
+    >>> sub=Subtract(a=3,b=1)
+    >>> sub.a.value
+    3
+    >>> sub.b.value
+    1
+    >>> sub.c.value
+    2
+
+
+    >>> v3=variable_operation(Subtract,v1,v2)
+    >>> v3.value
+    2
+
+    >>> v1.value=0
+    >>> v3.value
+    -1
+    """
+    a=algorithm()
+    for input,algorithm_input in zip(inputs,a.inputs):
+        if isinstance(input,Variable):
+            algorithm_input.track_variable(input)
+        else:
+            algorithm_input.value=input
+
+    return a.outputs[0]
 
 def __test_propagation():
     """
@@ -576,43 +769,53 @@ def __test_propagation():
 
 def __test_algorithm():
     """
-    >>> class AddOne(Algorithm):
-    ...    _inputs_=('input',)
-    ...    _outputs_=('output',)
-    ...    def update(self):
-    ...        self.output.value=1+(self.input.value or 0)
-    ...
-    ...    def __init__(self):
-    ...        self.input=Variable(0)
-    ...        Algorithm.__init__(self,enabled=True)
+        >>> class AddOne(Algorithm):
+        ...    _inputs_=('input',)
+        ...    _outputs_=('output',)
+        ...    def update(self):
+        ...        self.output.value=1+(self.input.value or 0)
+        ...
+        ...    def __init__(self,input=0):
+        ...        Algorithm.__init__(self,enabled=True,input=input)
 
-    
-    >>> i=AddOne()
-    >>> debugVariable(i.output,"i.output")
-    >>> i.input.value=1
-    i.output value: 2
-    >>> with i.input.updates_coalesced():
-    ...     i.input.value=3
-    ...     i.input.value=4
-    i.output blocked: True value: [2]
-    i.output value: 5
-    i.output blocked: False value: [5]
+        
+        >>> i=AddOne()
+        >>> debugVariable(i.output,"i.output")
+        >>> i.input.value=1
+        i.output value: 2
+        >>> with i.input.updates_coalesced():
+        ...     i.input.value=3
+        ...     i.input.value=4
+        i.output blocked: True value: [2]
+        i.output value: 5
+        i.output blocked: False value: [5]
+
+    Shows how to initialize variables
+        >>> j=AddOne(input=3)
+        >>> j.output.value
+        4
+
     """
- 
 
-def pp(name):
-    def p(x):
-        print "%s%s: %r"%(('-')*_nest_level,name,x)
-        return x
-    return p
-    
-def debugVariable(v,name):
-    def blockWatch(b):
-        assert(v.value == v.pendingValue) # Someone tried to manipulate blocked flag directly
-        print (('-') * _nest_level) + name + " blocked: " + repr(b) + (" value: [%r]"%(v.value,))
-    v.observers.insert(0,pp(name + " value"))
-    v.blocked.observers.insert(0,blockWatch)
-    
+def __test_variable_operations():
+    """
+        >>> v1=Variable(0)
+        >>> debugVariable(v1,"v1")
+        >>> v2=v1+1
+        >>> debugVariable(v2,"v2")
+        >>> v1.value=10    
+        v1 value: 10
+        v2 value: 11
+
+        >>> v3=Variable(0)
+        >>> v4=v1*v3
+        >>> debugVariable(v4,"v4")
+        >>> v3.value=5
+        v4 value: 50
+
+    """
+def any_var_is_none(l):
+    return any(map(lambda i: i.value is None,l))
 
 
 if __name__ == "__main__":
